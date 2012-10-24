9e356823e46eb667ddd2100ad80e2f6d8366765b
John Goodall <jgoodall@ornl.gov>
2012-10-24 13:51:50 -0400

    fixed initial table sort

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -20,21 +20,20 @@ $().ready(function() {
     return row;
   }
 
-  $.getJSON('data/endogenous.json', function(data) {
-    $.each(data, function(index, row) {
-      $('#endog-table tbody').append(parseRow(row));
-    });
-    // $('#endog-table').tablesorter( );
-  })
-
+  // set up table sorter, but disable the third column
+  $('#exog-table').tablesorter({headers: {2: {sorter: false}}});
+  
+  // set up tooltips for table headers - text in markup
+  $('#exog-table thead tr th span').tooltip();
+  
+  // async load table data and reorder on first and second columns
   $.getJSON('data/exogenous.json', function(data) {
     $.each(data, function(index, row) {
       $('#exog-table tbody').append(parseRow(row));
     });
-    $('#exog-table').tablesorter( [[0,0], [1,0]] );
+    $("#exog-table").trigger("update");
+    $("table").trigger("sorton",[[0,0],[1,0]]);
   })
 
-  $('#endog-table thead tr th span').tooltip();
-  $('#exog-table thead tr th span').tooltip();
 
 });
