f18053d065c23f46d71ca2e7017f2e2286a7fbda
John Goodall <jgoodall@ornl.gov>
2012-10-24 14:44:43 -0400

    only single data source file now, exog or endog is in json

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -7,12 +7,14 @@ $().ready(function () {
     var name = value.name || ''
       , url = value.url || ''
       , type = value.type || ''
+      , loc = value.location || ''
       , infoUrl = value.info || ''
       , desc = value.description || ''
       , categories = value.categories || []
     var row = 
         '<tr>'
       + '<td><a href="' + infoUrl + '">' + name + '</a></td>'
+      + '<td>' + loc + '</td>'
       + '<td>' + type + '</td>'
       + '<td>' + desc + '<br><strong>URL:</strong> <a href="' + url + '">' + url + '</a></td>'
       + '<td>' + categories.toString() + '</td>'
@@ -21,24 +23,24 @@ $().ready(function () {
   }
 
   // set up tooltips for table headers - text in markup
-  $('#exog-table thead tr th span').tooltip();
+  $('#datasource-table thead tr th span').tooltip();
   
   // async load table data and reorder on first and second columns
-  $.getJSON('data/exogenous.json', function (data) {
+  $.getJSON('data/sources.json', function (data) {
     $.each(data, function (index, row) {
-      $('#exog-table tbody').append(parseRow(row));
+      $('#datasource-table tbody').append(parseRow(row));
     });
 
     // set up table sorter, but disable the third column
-    $('#exog-table').tablesorter({
+    $('#datasource-table').tablesorter({
       debug: true,
       headers: {2: {sorter: false}}
     });
 
     // update table sort
-    $('#exog-table').trigger('update');
+    $('#datasource-table').trigger('update');
     var sortOrder = [[0, 0], [1, 0]]
-    $('#exog-table').trigger('sorton', [sortOrder]);
+    $('#datasource-table').trigger('sorton', [sortOrder]);
     
   })
 
